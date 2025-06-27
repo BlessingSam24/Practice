999e6fc1ff488eb6fc1ff3793e53aca817756143
BlessingSam24 <iblessingsam@gmail.com>
2025-06-27 15:17:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 console.log("Tested")
 console.log("I'm just clarifying my doubts")
 
-console.log(" after stash")
+console.log(" after stash")console.log(" after stash")
+console.log("yeah!... stashing is working")
